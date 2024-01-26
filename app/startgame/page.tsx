d65c7d23780bacb9ab6741a6ecd9efcaa12f710a
Davidson-09 <davidsonakra@gmail.com>
2024-01-26 22:06:20 +0100

    fixed merge issue

--- app/startgame/page.tsx
+++ app/startgame/page.tsx
@@ -1,96 +1,71 @@
+'use client'
 
-"use client";
+import React, { useEffect, useState } from 'react'
+import Header from '../components/Header'
+import GameBoard from '../components/GameBoard'
+import Button from '../components/Button'
+import WordInputGrid from '../components/WordInputGrid'
+import WordCompareGrid from '../components/WordCompareGrid'
 
-import React from "react";
-import Header from "../components/Header";
-import GameBoard from "../components/GameBoard";
-import Button from "../components/Button";
+interface Guess{
+    wordGuessed: string,
+}
 
-import {
-  useAccount,
-  useConnect,
-  //   useContract,
-  useContractRead,
-  useContractWrite,
-  useNetwork,
-  useWaitForTransaction,
-} from "wagmi";
-
-import { SINGLE_CONTRACT_ADDRESS } from "../smart-contract/constants";
-
-import CONTRACT_ABI2 from "../smart-contract/wordanamain2-abi.json";
+const RenderEmptyWordGrid = (props: { numberOfTimes:number }) => {
+    const {numberOfTimes} = props
+    const renderItems = () => {
+      const items = [];
+      for (let i = 0; i < numberOfTimes; i++) {
+        items.push(<WordInputGrid isActive={false} key={i} />);
+      }
+      return items;
+    };
+  
+    return <div className='flex flex-col items-center gap-3 m-4 mb-16'>{renderItems()}</div>;
+  };
 
 const Game = () => {
-  const {
-    data: stakeData,
-    write: guessWord,
-    isLoading: isStakeLoading,
-    isSuccess: isStakeStarted,
-    error: stakeError,
-  } = useContractWrite({
-    address: SINGLE_CONTRACT_ADDRESS,
-    abi: CONTRACT_ABI2,
-    functionName: "guessWord",
-  });
 
-  console.log(stakeData);
+    const [wordToGuess, setWordToGuess] = useState('hello')
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState<Guess[]>([])
 
-  const submitWord = async () => {
-    guessWord({
-      args: ["water"],
-    });
-  };
-  return (
+    const handleSubmission =()=>{
+        if (currentGuess.length === 5){
+            let prevGuesses = guesses
+            const newGuess: Guess = {wordGuessed: currentGuess}
+            prevGuesses.push(newGuess)
+            setGuesses(prevGuesses)
+            setCurrentGuess('')
+        } else{
+            alert('submitted word must have 5 letters')
+        }
+    }
+
+    return (
     <div>
-      <Header />
-      <div>
-        <div className="flex flex-col items-center gap-3 m-4 mb-16">
-          <div className="flex items-center mt-2 gap-3">
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-          </div>
-          <div className="flex items-center mt-2 gap-3">
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-          </div>
-          <div className="flex items-center mt-2 gap-3">
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-          </div>
-          <div className="flex items-center mt-2 gap-3">
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-          </div>
-          <div className="flex items-center mt-2 mb-2 gap-3">
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-            <input className="box rounded-lg bg-transparent border p-6 uppercase" />
-          </div>
-          <div onClick={submitWord}>
-            <Button title="Submit" />
-          </div>
-
+        <Header />
+        <div>
+            <div className='flex flex-col items-center gap-3 m-4 mb-16'>
+                {
+                    guesses.map((guess, index)=>(
+                        <div key={index}>
+                        {index > guesses.length - 6 && <WordCompareGrid wordGuessed={guess.wordGuessed} wordToGuess={wordToGuess}/>}
+                        </div>
+                    ))
+                }
+                <WordInputGrid isActive setGuess={setCurrentGuess} guess={currentGuess}/>
+                {
+                    guesses.length < 6 && <RenderEmptyWordGrid numberOfTimes={5-guesses.length}/>
+                }
+                <div onClick={()=>handleSubmission()}>
+                    <Button title='Submit'/>
+                </div>
+            </div>
+            <GameBoard />
         </div>
-        <GameBoard />
-      </div>
     </div>
+    )
+}
 
-  );
-};
-
-
-export default Game;
+export default Game
